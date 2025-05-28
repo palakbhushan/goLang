8772c82541d19235bbe1cc417663d27fd74f9c2f
palak bhushan <palakbosw@gmail.com>
2025-05-28 19:06:14 +0530

    slice in gfo

--- goBasics/array.go
+++ goBasics/array.go
@@ -4,33 +4,33 @@ import "fmt"
 
 func main() {
 
-	// var arrayName [size]elementType
+	var arrayName [size]elementType
 
-	// var numbers [5]int
-	// fmt.Println(numbers)
+	var numbers [5]int
+	fmt.Println(numbers)
 
-	// numbers[4] = 20
-	// fmt.Println(numbers)
+	numbers[4] = 20
+	fmt.Println(numbers)
 
-	// numbers[0] = 9
-	// fmt.Println(numbers)
+	numbers[0] = 9
+	fmt.Println(numbers)
 
-	// fruits := [4]string{"Apple", "Banana", "Orange", "Grapes"}
-	// fmt.Println("Fruits array:", fruits)
+	fruits := [4]string{"Apple", "Banana", "Orange", "Grapes"}
+	fmt.Println("Fruits array:", fruits)
 
-	// fmt.Println("Third element:", fruits[2])
+	fmt.Println("Third element:", fruits[2])
 
-	// originalArray := [3]int{1, 2, 3}
-	// copiedArray := originalArray
+	originalArray := [3]int{1, 2, 3}
+	copiedArray := originalArray
 
-	// copiedArray[0] = 100
+	copiedArray[0] = 100
 
-	// fmt.Println("Original array:", originalArray)
-	// fmt.Println("Copied array:", copiedArray)
+	fmt.Println("Original array:", originalArray)
+	fmt.Println("Copied array:", copiedArray)
 
-	// for i := 0; i < len(numbers); i++ {
-	// 	fmt.Println("Element at index,", i, ":", numbers[i])
-	// }
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println("Element at index,", i, ":", numbers[i])
+	}
 
 	// underscore is blank identifier, used to store unused values
 	for _, v := range numbers {
@@ -45,13 +45,13 @@ func main() {
 
 	fmt.Println("Array1 is equal to Array2:", array1 == array2)
 
-	// var matrix [3][3]int = [3][3]int{
-	// 	{1, 2, 3},
-	// 	{4, 5, 6},
-	// 	{7, 8, 9},
-	// }
+	var matrix [3][3]int = [3][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
 
-	// fmt.Println(matrix)
+	fmt.Println(matrix)
 
 	originalArray := [3]int{1, 2, 3}
 	var copiedArray *[3]int
