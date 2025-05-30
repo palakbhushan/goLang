581a18b201bfe38109affeee0cb9cb7e40960083
palak bhushan <palakbosw@gmail.com>
2025-05-30 05:54:01 +0530

    recursion in go generic

---
+++ goIntermediate/recursion.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(factorial(5))
+	fmt.Println(factorial(10))
+
+	fmt.Println(sumOfDigits(9))
+	fmt.Println(sumOfDigits(12))
+	fmt.Println(sumOfDigits(12345))
+
+	fmt.Println(fibo(3))
+
+}
+
+func factorial(n int) int {
+	// Base case: factorial of 0 is 1
+	if n == 0 {
+		return 1
+	}
+	// Recursive case: factorial of n is n * factorial(n - 1)
+	return n * factorial(n-1)
+	// n * (n - 1) * (n-2) * factorial (n-3)..... factorial(0)
+
+}
+
+func sumOfDigits(n int) int {
+	// Base case
+	if n < 10 {
+		return n
+	}
+	return n%10 + sumOfDigits(n/10)
+}
+
+func fibo(n int) int {
+	if n<=1 {
+		return n
+	}
+
+	return fibo(n-1) + fibo(n-2)
+}
